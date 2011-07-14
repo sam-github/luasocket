84c955a51bbe3a4559d553143cf873048c69eff8
Liam Devine <dmail00@gmail.com>
2011-07-14 13:50:16 +0100

    use load if 5.2 and loadstring if lua version < 5.2

--- test/testsrvr.lua
+++ test/testsrvr.lua
@@ -3,6 +3,12 @@ host = host or "localhost";
 port = port or "8383";
 server = assert(socket.bind(host, port));
 ack = "\n";
+
+local lua_load_func = loadstring
+if _VERSION == 'Lua 5.2' then
+	lua_load_func = load
+end
+
 while 1 do
     print("server: waiting for client connection...");
     control = assert(server:accept());
@@ -10,6 +16,6 @@ while 1 do
         command = assert(control:receive());
         assert(control:send(ack));
         print(command);
-		(load(command))();
+		(lua_load_func(command))();
     end
 end
